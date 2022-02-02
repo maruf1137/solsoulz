73b44f9cf7330df4ad874e59b4014be1d988aa37
maruf <mmaruf1137@gmail.com>
2022-02-02 14:45:37 +0600

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,70 @@
-$('.owl-carousel').owlCarousel({
-  loop:true,
-  margin:27,
-  nav:true,
+// caresol with jaquery
+
+$(".owl-carousel-rulls").owlCarousel({
+  loop: true,
+  margin: 27,
+  nav: true,
   dots: false,
-   navText: [
-     `<svg><use xlink:href="img/icons.svg#icon-arrow_back_ios"></use></svg>`,
+  navText: [
+    `<svg><use xlink:href="img/icons.svg#icon-arrow_back_ios"></use></svg>`,
     `<svg><use xlink:href="img/icons.svg#icon-arrow_forward_ios"></use></svg>`,
   ],
-  responsive:{
-      0:{
-          items:1
-      },
-      440:{
-          items:2
-      },
-      643:{
-          items:3
-      },
+  responsive: {
+    0: {
+      items: 1,
+    },
+    440: {
+      items: 2,
+    },
+    643: {
+      items: 3,
+    },
 
-      1200:{
-          items:4
-      },
-  }
+    1200: {
+      items: 4,
+    },
+  },
+});
+
+$(".owl-carousel-team").owlCarousel({
+  loop: true,
+  margin: 27,
+  nav: true,
+  dots: false,
+  navText: [
+    `<svg><use xlink:href="img/icons.svg#icon-arrow_back_ios"></use></svg>`,
+    `<svg><use xlink:href="img/icons.svg#icon-arrow_forward_ios"></use></svg>`,
+  ],
+  responsive: {
+    0: {
+      items: 1,
+    },
+    550: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
+
+
+// faqs
+const btns = document.querySelectorAll(".faqs__btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const parentEl = e.currentTarget.parentElement;
+    parentEl.classList.toggle("show-text");
+    console.log(parentEl);
+  });
+});
+
+// mobile nav
+const mblNavBtn =  document.querySelector('.nav-bar-btns')
+const navContainer = document.querySelector('.home__nav')
+
+mblNavBtn.addEventListener('click', function () {
+  navContainer.classList.toggle('show-mbl-nav')
+  mblNavBtn.classList.toggle('show-mbl-nav')
 })
-// $(".owl-carousel").owlCarousel({
-//   loop: false,
-//   margin: 20,
-//   nav: true,
-//   // navText: [
-//   //   `<svg><use xlink:href="img/icons.svg#icon-chevron-thin-left"></use></svg>`,
-//   //   `<svg><use xlink:href="img/icons.svg#icon-chevron-thin-right"></use></svg>`,
-//   // ],
-//   responsive: {
-//     0: {
-//       items: 4,
-//     },
-//   },
-// });
